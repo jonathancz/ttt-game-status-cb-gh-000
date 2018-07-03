dd0417fc1fb2a4a36e8b685b88900489c9fe9947
Ethan Chen <jonathanchenzheng@hotmail.com>
2018-07-03 02:27:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,11 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0, 1, 2] #Top row
+  [3, 4, 5] #Middel row
+  [6 ,7, 8] #Bottom row
+]
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win
